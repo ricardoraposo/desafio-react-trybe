9331353ee35e3e9ee68aa22a7734b3585e7101cc
Ricardo Raposo <ricardorbxx1@gmail.com>
2023-11-23 13:17:37 -0300

    uploading a few tests

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,10 +1,36 @@
-// import { renderWithRouter } from '../utils/renderWithRouter';
-import { render, screen } from '@testing-library/react';
+import { vi } from 'vitest';
+import { act, render, screen } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { newsMock } from './mocks/newsMock';
 import App from '../App';
 
-it('should show "Vite + React" text', () => {
-  render(<App />);
-  // renderWithRouter(<App />);
+const queryClient = new QueryClient();
 
-  expect(screen.getByText(/vite \+ react/i)).toBeInTheDocument();
+const fetchMock = { json: async () => newsMock } as Response;
+
+describe('Tests related with the header component', () => {
+  it('Check if header title shows up correctly', () => {
+    render(<QueryClientProvider client={ queryClient }><App /></QueryClientProvider>);
+
+    const title = screen.getByText(/Trybe News/i);
+
+    expect(title).toBeInTheDocument();
+  });
+});
+
+describe('Tests related with the main news component', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('Tests if title is loaded correctly', async () => {
+    vi.spyOn(global, 'fetch').mockResolvedValue(fetchMock);
+    await act(async () => {
+      render(<QueryClientProvider client={ queryClient }><App /></QueryClientProvider>);
+    });
+
+    const newsTitle = await screen.findByText(/IBGE/);
+
+    expect(newsTitle).toBeInTheDocument();
+  });
 });
